16790ab6aeb055e29d606d934ddfdf7a48b032eb
Michael Baird <mbaird@users.noreply.github.com>
2023-04-23 15:55:23 +0100

    Tweak inactive `StatusLine`

--- lua/monochrome/theme.lua
+++ lua/monochrome/theme.lua
@@ -94,7 +94,7 @@ function M.load_syntax()
 
     ModeMsg = {},
     MoreMsg = {},
-    StatusLine = {},
+    StatusLine = { bg = colors.bg_alt },
     StatusLineNC = {},
 
     MatchParen = { style = 'bold' },
